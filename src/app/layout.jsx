ad13241e7d1dc99db6c6a19740e300fe9fca0fe1
BryanTevillo <tevillo24@gmail.com>
2024-02-09 05:07:36 -0600

    Se realiza el footer

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,14 @@
+import Footer from "./(componentes)/Footer";
+
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="min-h-screen">
+        {children}
+        <Footer></Footer>
+      </body>
     </html>
   );
 }
