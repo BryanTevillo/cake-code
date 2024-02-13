a0bc354ae7971b4d555c014a439b386d250c3886
FernandaG <fernanda.guemez@gmail.com>
2024-02-13 13:16:45 -0600

    se modifican cards de cupcakes

--- src/app/Categorias/cupcakes/page.jsx
+++ src/app/Categorias/cupcakes/page.jsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { Card } from "flowbite-react";
 import { Luckiest_Guy } from "next/font/google";
 const luckiestguy = Luckiest_Guy({ subsets: ["latin"], weight: ["400"] });
 import "./styles.css";
@@ -15,15 +18,16 @@ export default function Cupcakes() {
       <h1 className={luckiestguy.className} id="titulo">
         CUP CAKES
       </h1>
-      <div className="grid justify-items-center md:grid-cols-3">
+
+      <div className="grid md:grid-cols-2 sm:grid-cols-3">
         {/* card1 */}
-        <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <Image className="p-5 rounded-t-lg" src={cupcake1} alt="cupcake1" />
-          <div className="p-1">
-            <h5 className="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Vainilla con chantilli
+        <div>
+          <Card className="max-w-sm">
+            <Image src={cupcake1} alt="cupcake1" />
+            <h5 className="text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Cupcake con chantilli sabor algodon de azucar
             </h5>
-            <p className="text-center mb-3 font-normal dark:text-gray-400">
+            <p className="text-center font-normal text-gray-700 dark:text-gray-400">
               $35.00 c/u
             </p>
             <div class="flex justify-center">
@@ -34,64 +38,17 @@ export default function Cupcakes() {
                 Agregar al carrito
               </button>
             </div>
-          </div>
+          </Card>
         </div>
-        {/* terminar card1 */}
-
+        {/* termino card1 */}
         {/* card2 */}
-        <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <Image className="p-5 rounded-t-lg" src={cupcake2} alt="cupcake1" />
-          <div className="p-1">
-            <h5 className="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        <div>
+          <Card className="max-w-sm">
+            <Image src={cupcake2} alt="cupcake2" />
+            <h5 className="text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               Avellana y ferrero rocher
             </h5>
-            <p className="text-center mb-3 font-normal dark:text-gray-400">
-              $50.00 c/u
-            </p>
-            <div class="flex justify-center">
-              <button
-                type="submit"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Agregar al carrito
-              </button>
-            </div>
-          </div>
-        </div>
-        {/* terminar card2 */}
-
-        {/* card3 */}
-        <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 resize-y">
-          <div>
-            <Image className="p-5 rounded-t-lg" src={cupcake3} alt="cupcake1" />{" "}
-          </div>
-          <div className="p-1">
-            <h5 className="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Café con chocolate
-            </h5>
-            <p className="text-center mb-3 font-normal dark:text-gray-400">
-              $45.00 c/u
-            </p>
-          </div>
-          <div class="flex justify-center">
-            <button
-              type="submit"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Agregar al carrito
-            </button>
-          </div>
-        </div>
-        {/* terminar card3 */}
-
-        {/* card4 */}
-        <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <Image className="p-5 rounded-t-lg" src={cupcake4} alt="cupcake1" />
-          <div className="p-1">
-            <h5 className="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Cereza con chispas de colores
-            </h5>
-            <p className="text-center mb-3 font-normal dark:text-gray-400">
+            <p className="text-center font-normal text-gray-700 dark:text-gray-400">
               $35.00 c/u
             </p>
             <div class="flex justify-center">
@@ -102,19 +59,18 @@ export default function Cupcakes() {
                 Agregar al carrito
               </button>
             </div>
-          </div>
+          </Card>
         </div>
-        {/* terminar card4 */}
-
-        {/* card5 */}
-        <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <Image className="p-5 rounded-t-lg" src={cupcake5} alt="cupcake1" />
-          <div className="p-1">
-            <h5 className="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Cacao y crema
+        {/* termino card2 */}
+        {/* card3 */}
+        <div>
+          <Card className="max-w-sm">
+            <Image src={cupcake3} alt="cupcake3" />
+            <h5 className="text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Café con chocolate
             </h5>
-            <p className="text-center mb-3 font-normal dark:text-gray-400">
-              $40.00 c/u
+            <p className="text-center font-normal text-gray-700 dark:text-gray-400">
+              $35.00 c/u
             </p>
             <div class="flex justify-center">
               <button
@@ -124,19 +80,60 @@ export default function Cupcakes() {
                 Agregar al carrito
               </button>
             </div>
-          </div>
+          </Card>
         </div>
-        {/* terminar card5 */}
-
+        {/* termino card3 */}
+        {/* card4 */}
+        <div>
+          <Card className="max-w-sm">
+            <Image src={cupcake4} alt="cupcake4" />
+            <h5 className="text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Cereza con chispas de colores
+            </h5>
+            <p className="text-center font-normal text-gray-700 dark:text-gray-400">
+              $35.00 c/u
+            </p>
+            <div class="flex justify-center">
+              <button
+                type="submit"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Agregar al carrito
+              </button>
+            </div>
+          </Card>
+        </div>
+        {/* termino card4 */}
+        {/* card5 */}
+        <div>
+          <Card className="max-w-sm">
+            <Image src={cupcake5} alt="cupcake5" />
+            <h5 className="text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Cupcake de oreo
+            </h5>
+            <p className="text-center font-normal text-gray-700 dark:text-gray-400">
+              $35.00 c/u
+            </p>
+            <div class="flex justify-center">
+              <button
+                type="submit"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Agregar al carrito
+              </button>
+            </div>
+          </Card>
+        </div>
+        {/* termino card5 */}
         {/* card6 */}
-        <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <Image className="p-5 rounded-t-lg" src={cupcake6} alt="cupcake1" />
-          <div className="p-1">
-            <h5 className="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        <div>
+          <Card className="max-w-sm">
+            <Image src={cupcake6} alt="cupcake6" />
+            <h5 className="text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               Red velvet
             </h5>
-            <p className="text-center mb-3 font-normal dark:text-gray-400">
-              $45.00 c/u
+            <p className="text-center font-normal text-gray-700 dark:text-gray-400">
+              $35.00 c/u
             </p>
             <div class="flex justify-center">
               <button
@@ -146,9 +143,9 @@ export default function Cupcakes() {
                 Agregar al carrito
               </button>
             </div>
-          </div>
+          </Card>
         </div>
-        {/* terminar card6 */}
+        {/* termino card6 */}
       </div>
     </div>
   );
