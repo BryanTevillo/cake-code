53d9e4e0f4c72dbbd39b514eb04bd90fe1fba9de
FernandaG <fernanda.guemez@gmail.com>
2024-02-16 15:57:42 -0600

    Actualizacion de modificaciones de card de producto

--- src/app/Categorias/Producto/page.jsx
+++ src/app/Categorias/Producto/page.jsx
@@ -1,12 +1,54 @@
 "use client";
-import { Card } from "flowbite-react";
+
 import { Open_Sans, Luckiest_Guy } from "next/font/google";
 const luckiestguy = Luckiest_Guy({ subsets: ["latin"], weight: ["400"] });
 const opensans = Open_Sans({ subsets: ["latin"] });
 import Image from "next/image";
 import Pastel4 from "../../../../public/Pastelcerezas.jpg";
+import { Rating } from "flowbite-react";
+import "./styles.css";
 
 export default function Producto() {
   return (
+    <div id="cuerpo">
+      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div className="md:flex">
+          <div className="md:shrink-0 grid justify-items-center">
+            <Image
+              src={Pastel4}
+              alt="pastel4"
+              className="w-[250px] h-[310px]"
+            />
+          </div>
+          <div className="p-8">
+            <div className="block mt-1 text-lg leading-tight font-medium text-black hover:underline grid justify-items-center">
+              Pastel de chocolate con decoración de cerezas
+            </div>
+            <a
+              href="#"
+              className="uppercase tracking-wide text-sm text-indigo-500 font-semibold"
+            >
+              $450.00
+            </a>
+            <Rating>
+              <Rating.Star />
+              <Rating.Star />
+              <Rating.Star />
+              <Rating.Star />
+              <Rating.Star filled={false} />
+              <p className="ml-2 text-sm font-medium text-gray-500 dark:text-gray-400">
+                4.95 out of 5
+              </p>
+            </Rating>
+            <p className="mt-2 text-slate-500">
+              Descripción: Delicioso pastel con relleno sabor chocolate y
+              chantilli en el exterior sabor cereza con decoracion de cerezas de
+              betún
+            </p>
+            <p className="mt-2 text-slate-500">Tamaño: Grande</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
---
+++ src/app/Categorias/Producto/prueba.jsx
@@ -0,0 +1,31 @@
+<Card
+      className="max-w-sm"
+      style={{
+        marginLeft: 100,
+        marginRight: 100,
+        marginTop: 50,
+        marginBottom: 50,
+        width: 500,
+        padding: 30,
+      }}
+      horizontal
+    >
+      <div className="flex flex-row space-x-4">
+        <div>
+          <Image
+            src={Pastel4}
+            alt="Pastel1"
+            className="h-48 w-full object-cover md:h-full md:w-48"
+          />
+        </div>
+        <div>
+          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Noteworthy technology acquisitions 2021
+          </h5>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+        </div>
+      </div>
+    </Card>*/
