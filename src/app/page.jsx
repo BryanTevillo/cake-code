c6955976c8b89d691b165b31d573191e5b1df61c
anapaulagarfias <paulagarfias4@gmail.com>
2024-02-17 19:58:59 -0600

    Se termina pagina de inicio, con medidas resposivas

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,7 +12,6 @@ import categoria1 from "../../public/cupcake/brownie.jpg";
 import categoria2 from "../../public/pastel-chocolate.jpg";
 import categoria3 from "../../public/galletas/amaranto.jpg";
 import categoria4 from "../../public/personaliza.jpg";
-import "./stylespaginaprincipal.css";
 const luckiestguy = Luckiest_Guy({ subsets: ["latin"], weight: ["400"] });
 import { Open_Sans, Luckiest_Guy } from "next/font/google";
 const opensans = Open_Sans({ subsets: ["latin"] });
@@ -22,16 +21,27 @@ export default function Home() {
     <main>
       <div>
         <div>
-          <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 border-none rounded-none">
-            <Carousel id="carrusel" slideInterval={2000}>
-              <Image className="max-width:100" src={carrusel1} alt="" />
-              <Image className="max-width:100" src={carrusel2} alt="" />
-              <Image className="max-width:100" src={carrusel3} alt="" />
-              <Image className="max-width:100" src={carrusel2} alt="" />
+          <div className="h-[220px] xl:h-[500px] md:h-[400px] 2xl:h-[700px]">
+            <Carousel slideInterval={10000}>
+              <Image
+                className="h-[220px] md:h-[400px] xl:h-[500px] 2xl:h-[700px]"
+                src={carrusel1}
+                alt="Prueba nuestro nuevo pastel de chocolate MILKA"
+              />
+              <Image
+                className="h-[220px] border-y-[22px] border-[#D8B4F8] md:border-0 xl:h-[500px] md:h-[400px] 2xl:h-[700px]"
+                src={carrusel2}
+                alt="Festeja tu boda con nosotros"
+              />
+              <Image
+                className="h-[220px] md:h-[400px] xl:h-[500px] 2xl:h-[700px]"
+                src={carrusel3}
+                alt="Ahora ya puedes personalizar tu pastel"
+              />
             </Carousel>
           </div>
         </div>
-        <div className="grid justify-items-center sm:h-[40rem] bg-[#ffb0cd] opacity-80">
+        <div className="grid justify-items-center xl:h-[40rem] bg-[#ffb0cd] opacity-80">
           <div
             className="font-bold text-lg"
             style={{ marginTop: 60, marginBottom: 30 }}
@@ -43,7 +53,7 @@ export default function Home() {
           </div>
           <div
             style={{ marginBottom: 50, marginTop: 30 }}
-            className="lg:px-80 text-center text-3xl"
+            className="xl:px-80 text-center text-3xl"
           >
             <p className={opensans.className}>
               Pastelería mexicana dedicada a consentir a los amantes de la
@@ -62,7 +72,7 @@ export default function Home() {
           </div>
         </div>
         <div>
-          <div className="border-[90px] sm:h-[50rem] border-[#FFD1BD] bg-[#FFDDCC]">
+          <div className="md:border-[70px] border-[50px] sm:h-[50rem] border-[#FFD1BD] bg-[#FFDDCC]">
             <div
               style={{ marginBottom: 50, marginTop: 30 }}
               className="text-5xl text-center"
@@ -78,11 +88,9 @@ export default function Home() {
                     className="max-w-sm bg-transparent border-none rounded-none shadow-none place-items-center"
                     renderImage={() => (
                       <Image
-                        width={200}
-                        height={200}
                         src={masvendido1}
                         alt="Pastel de oreo"
-                        className="rounded-[30px]"
+                        className="rounded-[30px] md:w-[200px] md:h-[300px] w-[200px] h-[280px]"
                       />
                     )}
                   >
@@ -103,11 +111,9 @@ export default function Home() {
                     className="max-w-sm bg-transparent border-none rounded-none shadow-none place-items-center"
                     renderImage={() => (
                       <Image
-                        width={200}
-                        height={200}
                         src={masvendido2}
                         alt="Cupcake de avellana"
-                        className="rounded-[30px]"
+                        className="rounded-[30px] md:w-[200px] md:h-[300px] w-[200px] h-[280px]"
                       />
                     )}
                   >
@@ -128,11 +134,9 @@ export default function Home() {
                     className="max-w-sm bg-transparent border-none rounded-none shadow-none place-items-center"
                     renderImage={() => (
                       <Image
-                        width={200}
-                        height={200}
                         src={masvendido3}
                         alt="Galletas de lavanda"
-                        className="rounded-[30px]"
+                        className="rounded-[30px] md:w-[200px] md:h-[300px] w-[200px] h-[280px]"
                       />
                     )}
                   >
@@ -153,11 +157,9 @@ export default function Home() {
                     className="max-w-sm bg-transparent border-none rounded-none shadow-none place-items-center"
                     renderImage={() => (
                       <Image
-                        width={200}
-                        height={200}
                         src={masvendido4}
                         alt="Cupcake de cafe"
-                        className="rounded-[30px]"
+                        className="rounded-[30px] md:w-[200px] md:h-[300px] w-[200px] h-[280px]"
                       />
                     )}
                   >
@@ -174,10 +176,10 @@ export default function Home() {
                   </Card>
                 </div>
               </div>
-              <div className="flex justify-center">
+              <div className="flex justify-center md:py-[25px]">
                 <button
                   type="submit"
-                  class="text-white text-xl bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-8 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:"
+                  class="text-white text-xl bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-8 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   <a href="/about">Ver más</a>
                 </button>
@@ -186,14 +188,14 @@ export default function Home() {
           </div>
         </div>
         <div>
-          <div className="justify-items-center md:border-x-[150px] border-y-[70px] md:rounded-[330px] md:h-[60rem] border-[#FFFFFF] bg-[#F3CCF3] min-h-full">
+          <div className="md:border-x-[55px] border-y-[70px] xl:rounded-[300px] md:rounded-[200px] md:h-[60rem] border-[#FFFFFF] bg-[#F3CCF3]">
             <div
               className="text-5xl text-center"
               style={{ marginBottom: 50, marginTop: 50 }}
             >
               <h1 className={luckiestguy.className}>Categorias</h1>
             </div>
-            <div className="grid justify-items-center md:grid-cols-2 px-48">
+            <div className="grid justify-items-center grid-cols-2 md:px-[110px]">
               <div>
                 <Card
                   className="max-w-sm bg-transparent border-none rounded-none shadow-none place-items-center"
@@ -201,7 +203,7 @@ export default function Home() {
                     <Image
                       src={categoria1}
                       alt="Cupcake de brownie"
-                      className="rounded-[30px] w-[235px] h-[240px]"
+                      className="rounded-[30px] md:w-[235px] md:h-[240px] w-[185px] h-[190px]"
                     />
                   )}
                 >
@@ -221,7 +223,7 @@ export default function Home() {
                     <Image
                       src={categoria2}
                       alt="pastel de chocolate"
-                      className="rounded-[30px] w-[235px] h-[240px]"
+                      className="rounded-[30px] md:w-[235px] md:h-[240px] w-[185px] h-[190px]"
                     />
                   )}
                 >
@@ -235,7 +237,7 @@ export default function Home() {
                 </Card>
               </div>
             </div>
-            <div className="grid justify-items-center md:grid-cols-2 px-48">
+            <div className="grid justify-items-center grid-cols-2 md:px-[110px]">
               <div>
                 <Card
                   className="max-w-sm bg-transparent border-none rounded-none shadow-none place-items-center"
@@ -243,7 +245,7 @@ export default function Home() {
                     <Image
                       src={categoria3}
                       alt="Galletas de amaranto"
-                      className="rounded-[30px] w-[235px] h-[240px]"
+                      className="rounded-[30px] md:w-[235px] md:h-[240px] w-[185px] h-[190px]"
                     />
                   )}
                 >
@@ -263,7 +265,7 @@ export default function Home() {
                     <Image
                       src={categoria4}
                       alt="Personaliza tu pastel"
-                      className="rounded-[30px] w-[235px] h-[240px]"
+                      className="rounded-[30px] md:w-[235px] md:h-[240px] w-[185px] h-[190px]"
                     />
                   )}
                 >
