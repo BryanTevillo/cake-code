6c4571e52f77d3835071e490a752a288ef108c99
BryanTevillo <tevillo24@gmail.com>
2024-02-19 12:58:55 -0600

    Se agrega cupcakes al dhasboard

--- src/app/Dashboard/Cupcakes/page.jsx
+++ src/app/Dashboard/Cupcakes/page.jsx
@@ -1,66 +1,63 @@
 "use client";
 
 import { Checkbox, Table, Button } from "flowbite-react";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 import "./style.css";
 function Cupcakes() {
   return (
     <>
-      <h1 className="text-center font-bold text-[5rem]">Lista Cupcakes</h1>
+      <h1 className="text-center font-bold text-[5rem]  mb-5">
+        Lista Cupcakes
+      </h1>
 
       <div>
-        <div className="grid md:grid-cols-2 sm:grid-cols-1 ">
-          <div className="justify-self-start">
-            <form className="w-[15rem]   md:w-[40rem]">
-              <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
-                Search
-              </label>
+        <div className="grid grid-cols-1 md:grid-cols-2   justify-items-center mb-10">
+          <form className="w-[20rem]    md:w-[30rem]  order-2 ">
+            <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
+              Search
+            </label>
 
-              <div className="relative">
-                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                  <svg
-                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                    />
-                  </svg>
-                </div>
-                <input
-                  type="search"
-                  id="default-search"
-                  className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Search Mockups, Logos..."
-                  required
-                />
-                <button
-                  type="submit"
-                  className="text-white  absolute end-2.5 bottom-2.5 bg-button_primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            <div className="relative">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg
+                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
                 >
-                  Search
-                </button>
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
               </div>
-            </form>
-          </div>
+              <input
+                type="search"
+                id="default-search"
+                className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search Mockups, Logos..."
+                required
+              />
+              <button
+                type="submit"
+                className="text-white  absolute end-2.5 bottom-2.5 bg-button_primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Search
+              </button>
+            </div>
+          </form>
 
-          <div className="grid md:justify-items-stretch sm:justify-content-center ">
-            <Button
-              gradientDuoTone="purpleToBlue"
-              className="max-w-[11rem] md:justify-self-end sm:justify-self-center"
-            >
-              + Agregar producto
-            </Button>
-          </div>
+          <Button gradientDuoTone="purpleToBlue" className="mb-5 md:mb-0">
+            + Agregar producto
+          </Button>
         </div>
 
-        <div className="overflow-x-auto">
+        <div className="overflow-x-auto mb-10">
           <Table hoverable>
             <Table.Head>
               <Table.HeadCell className="p-4">
@@ -72,6 +69,9 @@ function Cupcakes() {
               <Table.HeadCell>Price</Table.HeadCell>
               <Table.HeadCell>
                 <span className="sr-only">Edit</span>
+              </Table.HeadCell>
+              <Table.HeadCell>
+                <span className="sr-only">Delete</span>
               </Table.HeadCell>
             </Table.Head>
             <Table.Body className="divide-y">
@@ -90,7 +90,15 @@ function Cupcakes() {
                     href="#"
                     className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
                   >
-                    Edit
+                    <EditIcon></EditIcon>
+                  </a>
+                </Table.Cell>
+                <Table.Cell>
+                  <a
+                    href="#"
+                    className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                  >
+                    <DeleteIcon></DeleteIcon>
                   </a>
                 </Table.Cell>
               </Table.Row>
@@ -109,7 +117,15 @@ function Cupcakes() {
                     href="#"
                     className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
                   >
-                    Edit
+                    <EditIcon></EditIcon>
+                  </a>
+                </Table.Cell>
+                <Table.Cell>
+                  <a
+                    href="#"
+                    className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                  >
+                    <DeleteIcon></DeleteIcon>
                   </a>
                 </Table.Cell>
               </Table.Row>
@@ -128,7 +144,15 @@ function Cupcakes() {
                     href="#"
                     className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
                   >
-                    Edit
+                    <EditIcon></EditIcon>
+                  </a>
+                </Table.Cell>
+                <Table.Cell>
+                  <a
+                    href="#"
+                    className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                  >
+                    <DeleteIcon></DeleteIcon>
                   </a>
                 </Table.Cell>
               </Table.Row>
