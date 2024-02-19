d8d15e9294d82f4dad3cf36de4de24fa58518135
AlexLionP <alejandrodeleon904@gmail.com>
2024-02-19 02:30:24 -0600

    Pagina de usuario casi lista, ya tiene incorporada el historial

--- src/app/Usuario/page.jsx
+++ src/app/Usuario/page.jsx
@@ -3,6 +3,11 @@
 import { Button, Card, Checkbox, Label, TextInput } from 'flowbite-react';
 import { Open_Sans, Luckiest_Guy } from "next/font/google";
 import "./style.css";
+import Image from "next/image";
+import Pastel1 from "../../../public/pastel-oreo.jpg";
+import Pastel2 from "../../../public/pastel-chocolate.jpg";
+import Pastel3 from "../../../public/pastelbaby.jpg";
+import Pastel4 from "../../../public/Pastelcerezas.jpg";
 
 const opensans = Open_Sans({ subsets: ["latin"] });
 const luckiestguy = Luckiest_Guy({ subsets: ["latin"], weight: ["400"] });
@@ -115,12 +120,108 @@ function User() {
             ></path>
           </svg>
         </div>
-        <div className="h-[400px] grid justify-items-center" id="usuario2">
+        <div className="min-h-full md:h-[1700px] grid justify-items-center" id="usuario2">
         <div></div>
         <div className={luckiestguy.className} id ="titulo">
           TU HISTORIAL DE COMPRAS
         </div>
+        <div className="grid justify-items-center md:grid-cols-2 xl:grid-cols-3">
+        <div className={opensans.className}>
+            <Card className="max-w-xs my-4">
+              <Image
+                src={Pastel1}
+                alt="Pastel1"
+                className="w-[250px] h-[310px]"
+              />
+              <h5 className="text-center text-xl font-bold text-gray-900 dark:text-white">
+                Oreo con vainilla
+              </h5>
+              <p className="text-center font-normal text-gray-700 dark:text-gray-400">
+                $450.00
+              </p>
+              <div className="flex justify-center">
+                <button
+                  type="submit"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Comprar de nuevo
+                </button>
+              </div>
+            </Card>
+          </div>
+          <div>
+            <Card className="max-w-xs my-4">
+              <Image
+                src={Pastel2}
+                alt="Pastel2"
+                className="w-[250px] h-[310px]"
+              />
+              <h5 className="text-center text-xl font-bold text-gray-900 dark:text-white">
+                Chocolate con frutos rojos
+              </h5>
+              <p className="text-center font-normal text-gray-700 dark:text-gray-400">
+                $350.00
+              </p>
+              <div className="flex justify-center">
+                <button
+                  type="submit"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Comprar de nuevo
+                </button>
+              </div>
+            </Card>
+          </div>
+          <div>
+            <Card className="max-w-xs my-4">
+              <Image
+                src={Pastel3}
+                alt="Pastel3"
+                className="w-[280px] h-[310px]"
+              />
+              <h5 className="text-center text-xl font-bold text-gray-900 dark:text-white">
+                Chocolate para Baby shower
+              </h5>
+              <p className="text-center font-normal text-gray-700 dark:text-gray-400">
+                $250.00
+              </p>
+              <div className="flex justify-center">
+                <button
+                  type="submit"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Comprar de nuevo
+                </button>
+              </div>
+            </Card>
+          </div>
+          <div>
+            <Card className="max-w-xs my-4">
+              <Image
+                src={Pastel4}
+                alt="Pastel4"
+                className="w-[250px] h-[310px]"
+              />
+              <h5 className="text-center text-xl font-bold text-gray-900 dark:text-white">
+                Chocolate con cerezas
+              </h5>
+              <p className="text-center font-normal text-gray-700 dark:text-gray-400">
+                $450.00
+              </p>
+              <div className="flex justify-center">
+                <button
+                  type="submit"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Comprar de nuevo
+                </button>
+              </div>
+            </Card>
+          </div>
+
         </div>
+
+      </div>
 
         <div className="h-[12px]  md:h-[25px] xl:h-[30px]">
           <svg
