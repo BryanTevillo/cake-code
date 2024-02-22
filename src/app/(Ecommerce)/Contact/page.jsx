9b8986d6cfeb8aadca89f8a216ac59d675e4a491
anapaulagarfias <paulagarfias4@gmail.com>
2024-02-22 12:23:24 -0600

    Se corrigen botones para estandarizar diseño

--- src/app/(Ecommerce)/Contact/page.jsx
+++ src/app/(Ecommerce)/Contact/page.jsx
@@ -195,13 +195,13 @@ function page() {
                           <label for="galleta-relleno">Galleta</label>
                         </div>
                       </fieldset>
-                      <div class="flex justify-center">
-                        <button
-                          type="submit"
-                          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                      <div className="flex justify-center">
+                        <Button
+                          type="send"
+                          className="bg-button_primary px-2 py-1 text-center"
                         >
-                          Enviar
-                        </button>
+                          <p className="text-base">Enviar</p>
+                        </Button>
                       </div>
                     </form>
                   </div>
@@ -211,7 +211,7 @@ function page() {
           </div>
         </Card>
       </div>
-      <div className="h-[12px]  md:h-[25px] xl:h-[30px]">
+      <div className="h-[12px] md:h-[25px] xl:h-[30px]">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 5 1440 200"
