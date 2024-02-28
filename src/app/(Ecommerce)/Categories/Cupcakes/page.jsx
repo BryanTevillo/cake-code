f4682e6f1db9d038b7239d7f043aafd3713522ec
FernandaG <fernanda.guemez@gmail.com>
2024-02-28 13:39:45 -0600

    Se agrega componente search para pasteles cupcakes y galletas

--- src/app/(Ecommerce)/Categories/Cupcakes/page.jsx
+++ src/app/(Ecommerce)/Categories/Cupcakes/page.jsx
@@ -14,6 +14,7 @@ import cupcake4 from "../../../../../public/cupcake/cereza.webp";
 import cupcake5 from "../../../../../public/cupcake/oreo.webp";
 import cupcake6 from "../../../../../public/cupcake/redvelvet.webp";
 import Card1 from "@/app/components/CardCupcakes/CardCupcakes";
+import Search from "@/app/components/Search/search";
 
 export default function Cupcakes() {
   const [currentPage, setCurrentPage] = useState(1);
@@ -28,44 +29,7 @@ export default function Cupcakes() {
         <Waves color="#ffb0cd"></Waves>
       </div>
       <div>
-        <form className="w-[20rem]    md:w-[30rem]  order-2 mx-auto">
-          <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
-            Search
-          </label>
-
-          <div className="relative">
-            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-              <svg
-                className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
-            </div>
-            <input
-              type="search"
-              id="default-search"
-              className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Nombre del producto, sabor..."
-              required
-            />
-            <button
-              type="submit"
-              className="text-white absolute end-2.5 bottom-2.5 bg-button_primary hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Buscar
-            </button>
-          </div>
-        </form>
+        <Search></Search>
       </div>
       <div className="pt-50 grid justify-items-center md:grid-cols-3">
         <div className={opensans.className}>
